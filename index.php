3966f6eae96f67bc4554cb02d41431a1969e1c7a
Leonardo Villar Rodriguez <leorvr7@gmail.com>
2018-09-10 22:28:16 -0500

    if - else - elseif

--- index.php
+++ index.php
@@ -1,10 +1,26 @@
 <!doctype html>
 <html>
+
 <head>
-<meta charset="utf-8">
-<title>Documento sin título</title>
+	<meta charset="utf-8">
+	<title>Documento sin título</title>
 </head>
-	<?php?>
+
 <body>
+	
+	<?php
+	//Estructura Condicional if - else if - else
+	$nombre = "Leonardo";
+	$sexo = "S";
+	if ( $sexo == "M" ) {
+		$valor = "Bienvenido, " . $nombre;
+	} else if ( $sexo == "F" ) {
+		$valor = "Bienvenida, " . $nombre;
+	} else {
+		$valor = "Incognito";
+	}
+	print( $valor );
+	?>
 </body>
+
 </html>
