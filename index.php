929bdd31316a1d2da229302cc91fb642538beea8
Leonardo Villar Rodriguez <leorvr7@gmail.com>
2018-09-12 01:53:12 -0500

    Clase 1

--- index.php
+++ index.php
@@ -9,6 +9,42 @@
 <body>
 
 	<?php
+	//Mostrar Mensaje
+	echo "¡Hola, Soy un Script de PHP! <br>";
+	print "Hola mi primera clase";
+	print( "<p>Esto es un parrafo</p>" )
+	?>
+
+	<?php
+	//Comentario de una linea
+	/**
+	 *comentario de varias lineas
+	 *123
+	 *456
+	 *789
+	 */
+	?>
+
+	<?php
+	//Variables
+	print( "<article class=articulo -final>" );
+	$numero = 22;
+	$nombre = "Leo";
+	$valor = true;
+	$pi = 3.1416;
+	var_export( $pi );
+	print( "Tu nombre es: " . $nombre . " y tienes: " . $numero );
+	print( "</article>" );
+	?>
+
+	<?php
+	//Definir Constante
+	define("minombre","Leonardo");
+	define("miapellido", "Villar");
+	echo(minombre . " " . miapellido);
+	?>
+
+	<?php
 	//Estructura Condicional if - else if - else
 	$nombre = "Leonardo";
 	$sexo = "S";
@@ -19,26 +55,25 @@
 	} else {
 		$valor = "Incognito";
 	}
-	print( $valor . " <br>");
+	print( $valor . " <br>" );
 	?>
-	
-	
+
 	<?php
 	//Estructura condicion Switch
-	$opcion=3;
-	switch($opcion){
+	$opcion = 3;
+	switch ( $opcion ) {
 		case 1:
-			echo("Activa Opcion 1");
+			echo( "Activa Opcion 1" );
 			break;
 		case 2:
-			echo("Activa Opcion 2");
+			echo( "Activa Opcion 2" );
 			break;
 		default:
-			echo("Activa Opcion 3");
+			echo( "Activa Opcion 3" );
 			break;
 	}
 	?>
-	
+
 	<?php
 	//Estructura Repititiva While
 	$cont = 1;
@@ -63,13 +98,20 @@
 		print( "<br> Numero " . $contador );
 	}
 	?>
+
+	<?php
+	//Funcion
+	function Suma( $a, $b ) {
+		$suma = $a + $b;
+		return ( $suma );
+	}
+	$resultado = Suma( 5, 12 );
+	print( $resultado );
+	?>
 	
+	<?php
 	
-
-
-
-
-
+	?>
 
 </body>
 
